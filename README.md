d96a156e37c9501c2344939f5ce042450aa36082
Tony Lu <lutony78@gmail.com>
2016-10-15 17:43:13 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# session-internal #
+
+## Build & Run ##
+
+```sh
+$ cd session-internal
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/SessionInternal/SessionInternal.scala
@@ -0,0 +1,16 @@
+package SessionInternal
+
+import org.scalatra._
+
+class SessionInternal extends SessioninternalStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}

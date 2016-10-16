6ba33ca224094c309c7890dfbb82ebe07058a17b
Tony Lu <lutony78@gmail.com>
2016-10-16 16:56:17 -0400

    basic template for Apis

--- project/build.scala
+++ project/build.scala
@@ -27,6 +27,8 @@ object SessionServiceBuild extends Build {
         "org.scalatra" %% "scalatra" % ScalatraVersion,
         "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
         "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
+        "org.scalatra" %% "scalatra-json" % "2.4.0-RC2-2",
+        "org.json4s" %% "json4s-jackson" % "3.3.0.RC2",
         "ch.qos.logback" % "logback-classic" % "1.1.5" % "runtime",
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
--- src/main/scala/com/shuttleql/services/session/SessionServiceServlet.scala
+++ src/main/scala/com/shuttleql/services/session/SessionServiceServlet.scala
@@ -1,16 +1,52 @@
 package com.shuttleql.services.session
 
-import org.scalatra._
+import org.scalatra.json._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.Ok
 
-class SessionServiceServlet extends SessionServiceStack {
+class SessionServiceServlet extends SessionServiceStack with JacksonJsonSupport {
 
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
   }
 
+  get("/current") {
+  	// check for secret key
+  	// clubSessionId = ClubSession.id
+  	Ok()
+  }
+
+  post("/create") {
+  	// check for secret key
+  	// newSession = Session.create
+  	// ClubSession.start(newSession)
+  	Ok()
+  }
+
+  put("/end") {
+  	// check for secret key
+  	// ClubSession.end
+  	Ok()
+  }
+
+  post("/checkin") {
+  	// email = param[email]
+  	// password = param[password]
+
+  	// user = userService.findUser(email, password)
+  	// if user.password_hash == HashWithSalt(password)
+  	//		UserToSession.create(user.id, ClubSession.id, checkin: Time.now)
+  	//		OK()
+  	// else
+  	// 		NotFound()
+  	Ok()
+  }
+
+  put("/checkout") {
+  	// user_id = param[user_id]
+  	// UserToSession.update(ClubSession.id, user_id, checkout: Time.now)
+  	Ok()
+  }
 }
